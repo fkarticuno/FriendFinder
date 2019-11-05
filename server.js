64d3bfa0d844ca83b0dbd6290a559ad37637050e
Frederick Articuno <mckiyoshi@gmail.com>
2019-11-04 23:10:31 -0500

    everything is connected but isn't aware of modulating data

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var env = require("dotenv").config();
 var express = require("express");
 var path = require("path");
 
-console.log(process.env.key)
+// console.log(process.env.key)
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./app/routing/apiRoutes")(app);
-//require("./app/routing/htmlRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
